4e454030ebbec7dc1fa63a591a3386dbfd03a247
Arbi Hernowo <wariharbi@gmail.com>
2019-10-18 00:48:10 +0700

    Commit pertama

---
+++ kemampuan dasar/kemampuan-dasar-1/kasus/cari-java.ps1
@@ -0,0 +1,7 @@
+﻿tree /f D:\praxis-academy 
+Write-Host "";
+if (tree /f D:\praxis-academy *.java){
+    "Ada file Java pada direktori D:\praxis-academy\kemampuan-dasar-1\kasus\"
+    }
+
+Write-Host "";
---
+++ kemampuan dasar/kemampuan-dasar-1/kasus/file-ren.ps1
@@ -0,0 +1,24 @@
+﻿tree /f D:\praxis-academy 
+Write-Host "";
+if (tree /f D:\praxis-academy *.java){
+    "Ada file Java pada direktori D:\praxis-academy"
+    $search = Read-Host -Prompt 'Masukkan File yang ingin dicari:' 
+    $file = Get-ChildItem -Path D:\praxis-academy\kemampuan-dasar-1 -Recurse -Include "$($search).java" | Select-Object -First 1
+    if ($file) {
+    $choice = Read-Host -Prompt "$($search).java ada, mau ganti nama? (y/t)" 
+    if ( $choice -eq "y") {
+         $NewName = Read-Host -Prompt 'Silakan masukkan nama baru:' 
+         Rename-Item -Path $file -NewName $NewName
+         Write-Host "Nama file berhasil diganti!";
+         }
+    else {
+    Write-Host "Nama file tidak diganti"
+    }
+         
+      }
+    else {
+    Write-Host "File Tidak ada"
+    }
+}
+
+Write-Host "";
