83a283ad525a88f6184cca87118255442dabb930
Arbi Hernowo <wariharbi@gmail.com>
2019-11-05 16:14:36 +0700

    Update kasus hari-2

---
+++ Novice/01-02/Kasus/amstrongNumber.js
@@ -0,0 +1,20 @@
+function amstrongNumber () 
+{
+    for (var i = 0; i<10; ++i)
+    {
+        for (var j=0; j<10; ++j)
+        {
+            for (var k=0; j<10; ++k){
+                var pow = (Math.pow(i,3) + Math.pow(j,3) + Math.pow(k,3));
+                var plus = (i*100 + j * 10 + k );
+                if (pow == plus )
+                {
+                    alert(pow);
+                }
+            }    
+        
+        }
+    }
+}
+
+amstrongNumber();
---
+++ Novice/01-02/Kasus/gcd.js
@@ -0,0 +1,17 @@
+var a = 2154;
+var b = 458;
+var gcd;
+while (a!=b) 
+{
+    if (a>b)
+    {
+        a = a - b;
+    }
+    else 
+    {
+        b = b - a;
+    }
+}
+
+gcd = a;
+alert(gcd);
