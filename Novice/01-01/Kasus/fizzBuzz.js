a04b78280007941e75cf44ab25769b50ae2f5f12
Arbi Hernowo <wariharbi@gmail.com>
2019-11-05 15:23:32 +0700

    update

--- Novice/01-01/Kasus/fizzBuzz.js
+++ Novice/01-01/Kasus/fizzBuzz.js
@@ -1,6 +1,6 @@
 for (let i = 1;  i<= 100; i++) {
-let hasil = "";
-if (i % 3 == 0) hasil += "Fizz";
-if (i % 5 == 0) hasil += "Buzz";
+    let hasil = "";
+    if (i % 3 == 0) hasil += "Fizz";
+    if (i % 5 == 0) hasil += "Buzz";
 console.log(hasil || i);
 }
